20f26a4c8a8f07ff4bda5d03d0002532936c564c
emykion <emykion@gmail.com>
2019-02-08 22:22:25 -0500

    queue limit size

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
 var PORT = 3000;
 const Message = require("./server_constants.js")
-
 //app is a function handler
-var app = require("express")();
-
+var express = require("express");
+var app = express();
 //http is the actual server library
 var http = require("http").Server(app);
-
 //io is for sockets
 var io = require("socket.io")(http);
 
+const MAX_QUEUE_SIZE = 2;
+var clientQueue = [];//a queue of client sockets
+//unshift -> add to beginning of array
+//pop -> pop from back
+
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
-});
+    if(clientQueue.length >= MAX_QUEUE_SIZE){
+        res.sendFile(__dirname + "/client/full.html");
+    }else{
+        
+        res.sendFile(__dirname + "/client/index.html");
+    }
+    
+})
 
-app.get("/socket.io.slim.js", function(req, res){
-    res.sendFile(__dirname + "/client/socket.io.slim.js");
-});
-
-app.get("/three.min.js", function(req, res){
-    res.sendFile(__dirname + "/client/three.min.js");
-});
-
-app.get("/main.js", function(req, res){
-    res.sendFile(__dirname + "/client/main.js");
-});
-
-app.get("/client_constants.js", function(req, res){
-    res.sendFile(__dirname + "/client/client_constants.js");
-});
-
+app.use(express.static("client"));//looks in the client folder
 
 io.on(Message.CONNECTION, function(socket){
-    console.log("a user connected");
+    
+    if(clientQueue.length >= MAX_QUEUE_SIZE){
+        socket.disconnect(true);
+        return;    
+    }
+    
+    clientQueue.unshift(socket);
+    console.log("a user connected, " + clientQueue.length + " clients connected.");
+    
     
     socket.on(Message.WORLD, function(msg){
         
@@ -41,6 +43,16 @@ io.on(Message.CONNECTION, function(socket){
     
     socket.on(Message.DISCONNECT, function(){
         console.log("a user disconnected");
+        
+        for(var i = 0; i < clientQueue.length; i++){
+            
+            sock = clientQueue[i];
+            if(sock.id == socket.id){
+                clientQueue.splice(i, 1);
+                console.log("removed client from list");
+                break;
+            }
+        }
         
     });
     
